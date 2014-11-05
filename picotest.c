adc8707195075026d4e9df138dcd03e94e2190d3
Kazuho Oku <kazuhooku@gmail.com>
2014-11-06 06:27:03 +0900

    alloca is never used

--- picotest.c
+++ picotest.c
@@ -19,7 +19,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#include <alloca.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
